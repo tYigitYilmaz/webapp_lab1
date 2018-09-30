e8b4a856580f131d95c832c792c51055320e5c53
Yigit Yilmaz <tahayigityilmaz@gmail.com>
2018-09-30 16:20:34 +0300

    the empty project was init

---
+++ src/main/java/com/yilmaz/rsio/webapp_lab1/WebappLab1Application.java
@@ -0,0 +1,12 @@
+package com.yilmaz.rsio.webapp_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappLab1Application.class, args);
+    }
+}
---
+++ src/main/java/com/yilmaz/rsio/webapp_lab1/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.yilmaz.rsio.webapp_lab1.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @RequestMapping("/{name}")
+        private String helloUser(@PathVariable String name){
+            return "Hello "+ name;
+        }
+    }
